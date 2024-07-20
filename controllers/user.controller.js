980c11931b31da533ee62903a545d9d0764f35ee
Dmitrij Kolotusha <suvstreet@gmail.com>
2024-07-20 20:03:55 +0300

    порпавки

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,10 +1,8 @@
 const bcrypt = require('bcrypt')
 const User = require('../models/User')
 const { generate } = require('../helpers/token')
-const ROLES = require('../constants/roles')
 const chalk = require('chalk')
 
-// register
 async function register(email, password, created_at, updated_at) {
 	if (!password) {
 		throw new Error('Нужно ввести пароль!')
@@ -21,26 +19,27 @@ async function register(email, password, created_at, updated_at) {
 
 	const token = generate({ id: user._id })
 
-	console.log(chalk.bgGreen(`Пользователь ${user._id} успешно зарегистрировался!`))
+	console.log(chalk.bgGreen(`Пользователь ${user._id} успешно зарегистрировался`))
 
 	return { token }
 }
 
-// login
 async function login(email, password) {
-  const user = await User.findOne({ email })
+	const user = await User.findOne({ email })
 
-  if (!user) {
-    throw new Error('Пользователь не найден!')
-  }
+	if (!user) {
+		throw new Error('Пользователь не найден!')
+	}
 
-  const isPasswordCorrect = await bcrypt.compare(password, user.password)
+	const isPasswordCorrect = await bcrypt.compare(password, user.password)
 
-  if (!isPasswordCorrect) {
-    throw new Error('Пароль неверный!')
-  }
+	if (!isPasswordCorrect) {
+		throw new Error('Пароль неверный!')
+	}
 
-  return { token: generate({ id: user._id }) }
+	console.log(chalk.bgGreen(`Пользователь ${user._id} успешно авторизовался`))
+
+	return { token: generate({ id: user._id }) }
 }
 
 module.exports = {
