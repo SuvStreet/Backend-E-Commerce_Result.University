f49f2268f5d078108d0e64b1190de80d1eb17d24
Dmitrij Kolotusha <suvstreet@gmail.com>
2024-08-18 16:37:30 +0300

    fix helpers отправки на front

--- helpers/mapSubCategory.js
+++ helpers/mapSubCategory.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose')
-const mapProduct = require("./mapProduct")
+const mapProduct = require('./mapProduct')
 
 module.exports = function (subCategoryDb) {
 	return {
 		id: subCategoryDb._id,
 		name: subCategoryDb.name,
-		categoryId: subCategoryDb.category_id,
+		category: subCategoryDb.category_id.name,
 		imgUrl: subCategoryDb.img_url,
 		products: subCategoryDb.products.map((product) =>
 			mongoose.isObjectIdOrHexString(product) ? product : mapProduct(product),
