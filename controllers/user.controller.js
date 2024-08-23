c20d2cc98d7196600752098ebbde37aaaae54433
Dmitrij Kolotusha <suvstreet@gmail.com>
2024-08-23 16:21:37 +0300

    Получить роли и всех пользователей

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,5 +1,6 @@
 const User = require('../models/User')
 const chalk = require('chalk')
+const ROLE = require('../constants/roles')
 
 async function editUser(id, user) {
 	try {
@@ -23,6 +24,40 @@ async function editUser(id, user) {
 	}
 }
 
+async function getUserList() {
+	try {
+		const users = await User.find()
+
+		if (!users.length) {
+			throw new Error('Пользователи не найдены!')
+		}
+
+		console.log(chalk.bgGreen('Пользователи успешно получены'))
+
+		return users
+	} catch (err) {
+		console.log(
+			chalk.bgRed(`При получении пользователей пошло что-то не так: ${err.message}`),
+		)
+		throw new Error(err.message || 'Неизвестная ошибка...')
+	}
+}
+
+function getRoles() {
+	return [
+		{
+			id: ROLE.ADMIN,
+			name: 'Администратор',
+		},
+		{
+			id: ROLE.USER,
+			name: 'Пользователь',
+		},
+	]
+}
+
 module.exports = {
 	editUser,
+	getUserList,
+	getRoles,
 }
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,13 +1,35 @@
 const express = require('express')
 const authenticated = require('../middleware/authenticated')
 const mapUser = require('../helpers/mapUser')
-const { editUser } = require('../controllers/user.controller')
+const { editUser, getUserList, getRoles } = require('../controllers/user.controller')
+const hasRole = require('../middleware/hasRole')
+const ROLE = require('../constants/roles')
 
 const router = express.Router({ mergeParams: true })
 
 router.get('/', authenticated, async (req, res) => {
 	try {
 		res.send({ error: null, data: { user: mapUser(req.user) } })
+	} catch (err) {
+		res.send({ error: err.message || 'Неизвестная ошибка...', user: null })
+	}
+})
+
+router.get('/roles', authenticated, hasRole([ROLE.ADMIN]), async (req, res) => {
+	try {
+		const roles = await getRoles()
+
+		res.send({ error: null, data: roles })
+	} catch (err) {
+		res.send({ error: err.message || 'Неизвестная ошибка...', data: null })
+	}
+})
+
+router.get('/users', authenticated, hasRole([ROLE.ADMIN]), async (req, res) => {
+	try {
+		const users = await getUserList()
+
+		res.send({ error: null, data: { users: users.map(mapUser) } })
 	} catch (err) {
 		res.send({ error: err.message || 'Неизвестная ошибка...', user: null })
 	}
